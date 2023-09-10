d98fcfe018408a88f9740b1a733b8669065c2ba6
Dubem <dubemmegbo@gmail.com>
2023-09-10 01:11:41 +0100

    reg-ex1

---
+++ regex.py
@@ -0,0 +1,12 @@
+colors = ['blue', 'white', 'red', 'yellow']
+	
+fruit = ['blueberry','apple','cherry','banana']
+print(colors[0])
+
+print (colors[0],fruit[0], colors[1], fruit[1], colors[2], fruit[2], colors[3], fruit[3])
+
+import re
+
+
+print(re.findall(r'[Gg]eek', 'GeeksforGeeks: \
+				A computer science portal for geeks'))
